af38a8b7f7e3abb63dd8e49e9a12b51aca0bc82a
Aoyama, Shotaro <aosho235@gmail.com>
2017-08-03 07:28:56 +0900

    up

--- etc/install.sh
+++ etc/install.sh
@@ -6,28 +6,24 @@ ETC="$DOT/etc"
 
 mkdir -p ~/bin ~/tmp ~/.vimundo
 
-BACKUP_DIR="$HOME/dot/$(date +%Y%m%d%H%M%S)"
+BACKUP_DIR="$HOME/dot_backup"
+TIME=$(date +%Y%m%d_%H%M%S)
 
 link() {
     if [ -f "$HOME/$1" ]; then
         mkdir -p $BACKUP_DIR
-        mv "$HOME/$1" $BACKUP_DIR
+        mv -v "$HOME/$1" "$BACKUP_DIR/$1.$TIME"
     fi
-    ln -sf $ETC/$1 ~
+    ln -sfv $ETC/$1 ~
 }
-
-#echo "source $DOT/etc/.profile" >> ~/.profile
-#echo "source ~/.profile" >> ~/.bash_profile
-#echo "source $DOT/etc/.bashrc" >> ~/.bashrc
-#echo "source $DOT/etc/.alias" >> ~/.alias
-#echo "source $DOT/etc/.zshrc" >> ~/.zshrc
-#echo 'source $HOME/svn/etc/.screenrc' >> ~/.screenrc
 
 link .alias
 link .ctags
 link .gdbinit
 link .gdbinit.d
 link .gitignore
+link .irbrc
 link .sqliterc
+link .pryrc
 link .vim
 ln -sf $ETC/.vim/.vimrc ~/.vimrc
