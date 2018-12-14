8bddbf9e4d62ff0e69681f5b942576ae0638c3c4
Aoyama, Shotaro <aoyama@val.co.jp>
2018-12-14 11:00:18 +0900

    parse

---
+++ etc/.vim/template/ruby/parse_args.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require "optparse"
+
+def parse_args
+  @options = {}
+  opt = OptionParser.new
+  opt.banner = <<~EOF
+  サンプルスクリプト
+  EOF
+  opt.on("-f", "--format=FORMAT", "出力フォーマット（デフォルト：json）") {|v| @options[:format] = v }
+  opt.parse!(ARGV)
+end
+
+parse_args
+
+p @options
